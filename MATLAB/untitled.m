fb50cfeefeea2ab7e368ba5101f575bf71c54d56
VictorVarela <34323425+VictorVarela@users.noreply.github.com>
2018-11-16 16:45:03 -0200

    Delete untitled.m

--- MATLAB/untitled.m
+++
@@ -1,9 +0,0 @@
-clc; clear all; close all
-
-X = csvread ('~/Dropbox/Journal/VETOR_IMAGENS/img1_GFMT.csv');
-% classes = csvread('~/Dropbox/Dados/VETOR_IMAGENS/ProfOrdenadoGENERO.csv');
-
-
-XLabel = [zeros(10,1); ones(10,1)];
-
-[Prediction, PredictionTrain] = xvalPcaMldaLOO(X, XLabel, 0)
