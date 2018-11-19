a11c593b6c856207c63ad6161b15168a5888df36
Unknown <victor_varela@hotmail.com>
2018-11-19 16:14:46 -0200

    Update

--- MATLAB/testes.m
+++ MATLAB/testes.m
@@ -9,8 +9,5 @@ clc
 
 dado = csvread ('~/Dropbox/Journal/VETOR_IMAGENS/imgCONCATENADA_GFMT.csv');
 
+Graph_mlda_2D(dado, [115 230 115]);
 
-
-% USANDO TODOS OS DADOS PARA TREINAMENTO
-[P,K,V] = ctmlda(dado, 2,[230 230],1); % Calcula o MLDA
-Y = dado * P; % Projeta os dados nos Autovetores P
